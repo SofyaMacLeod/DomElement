b7ba1e48c53becf7211bbcae7ffe51a6e43d22e8
S Macleod <toma98so@gmail.com>
2021-09-27 00:59:41 +0300

    feat(l14.js): task 14 error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 'use sctrict';
 
-let domElement = function(selector,height,width, bg, fontSize) {
+let domElement = function(selector, height,width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
@@ -11,18 +11,18 @@ let domElement = function(selector,height,width, bg, fontSize) {
 
 domElement.prototype.create = function (){
   let div = document.createElement('div');
-  if (this.selector == '.') {
+  if (this.selector[0] == '.') {
     this.selector.classList.add('');
     document.body.appendChild(div);
   }
-  if (this.selector == '#'){
+  if (this.selector[0] == '#'){
     let p = document.createElement('p');
     this.selector.id = 'p';
     document.body.appendChild(p);
   }
-
-div.style.cssText = 'height: 26px; width: 45px; background-color: #435C89; font-size: 18px;';
+  div.style.cssText = 'height: 26px; width: 45px; background-color: #435C89; font-size: 18px;';
 };
+
  let test = new domElement('.dom', '48px', '26px', 'black', '24px');
  test.create('Для проверки кода');
  console.log(domElement);
